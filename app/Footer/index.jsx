2524123cc95dc97c1c7940e2b468a71e10011091
Mrmharvs <marvin.beltran17@gmail.com>
2024-09-10 18:45:50 +0800

    Assignment

---
+++ app/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>Ang gwapo ni Sir Larry</div>
+  )
+}
+
+export default footer
---
+++ app/HeroSection/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const herosection = () => {
+  return (
+    <div>Cute ni Sir Larry</div>
+  )
+}
+
+export default herosection
